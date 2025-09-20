aa8d8efe4d9be5626c709ef9909e1bbc35e35fc1
Nimsara Jayarathna <66525584+Nimsara-Jayarathna@users.noreply.github.com>
2025-09-20 12:44:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react(),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // your backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
