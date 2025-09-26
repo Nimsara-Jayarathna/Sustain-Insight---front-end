5b509b401fd75c0b0516723e85779962203deb06
Nimsara Jayarathna <nimsarajayarathna1@gmail.com>
2025-09-26 10:59:01 +0530

    minor fixes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,9 +4,9 @@ export const apiFetch = async (endpoint: string, options: RequestInit = {}) => {
   const token = localStorage.getItem("token");
 
   // build headers
-  const headers: HeadersInit = {
+  const headers: Record<string, string> = {
     "Content-Type": "application/json",
-    ...(options.headers || {}),
+    ...(options.headers as Record<string, string> || {}),
   };
 
   // attach token if not auth endpoint
